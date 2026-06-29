a8f8ebc591898f38456e744ff43f2d4859e98396
adu999 <adu999@users.noreply.github.com>
2026-06-29 21:14:05 +0800

    配置为 Keep 数据源，个性化站点信息

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,10 +15,10 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteTitle: '老杜跑步',
+  siteUrl: 'https://adu999.github.io/running_page',
+  logo: '',
+  description: '每一步都算数',
   navLinks: [
     {
       name: 'Summary',
@@ -26,11 +26,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://adu999.github.io',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/adu999/running_page',
     },
   ],
 };
